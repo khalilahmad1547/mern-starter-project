000c4d6f101a022a0ac9a7e8cc38a00e2a5fca36
Abdur Rahman <khalil.ahmad1547@gmail.com>
2022-09-17 00:06:10 +0500

    basic express app is created.

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+
+const userRouter = require("./api/routes/user_router");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api/users", userRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+});
+
+module.exports = app;
