d9547ce7f76b1dac0900c789eba48bf4d5288524
Abdur Rahman <khalil.ahmad1547@gmail.com>
2022-09-17 21:09:47 +0500

    login route is added to user routes

--- api/routes/user_router.js
+++ api/routes/user_router.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { signUpUser } = require("../controllers/user_controller");
+const { signUpUser, loginUser } = require("../controllers/user_controller");
 const router = express.Router();
 
 /* GET users listing. */
 router.post("/signup", signUpUser);
+router.post("/login", loginUser);
 
 module.exports = router;
