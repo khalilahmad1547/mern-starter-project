4197cbdbabd370e5e4cf610840c4c8e63f050dee
Abdur Rahman <khalil.ahmad1547@gmail.com>
2022-09-17 18:36:32 +0500

    a simple signup feature is added.

--- api/controllers/user_controller.js
+++ api/controllers/user_controller.js
@@ -1,5 +1,21 @@
-const signUpUser = (req, res) => {
-  res.status(200).json({ message: "sign up user request" });
+const { User, userExists } = require("../db/models/user_model");
+
+const signUpUser = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  const exist = await userExists(email);
+  if (exist) {
+    return res
+      .status(404)
+      .json({ message: `user with ${email} already exist` });
+  } else {
+    const user = new User({
+      fullName: fullName,
+      email: email,
+      password: password,
+    });
+    await user.save();
+    res.status(201).json(user);
+  }
 };
 
 const loginUser = (req, res) => {};
